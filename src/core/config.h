a946e33cc7e4268b4654d078249d629835b014fb
demian1839 <201471189+demian1839@users.noreply.github.com>
2026-09-16 16:22:04 +0200

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -61,7 +61,7 @@ public:
     // Wifi
     Credential webUI = {"admin", "bruce"};
     std::vector<String> webUISessions = {}; // FIFO queue of session tokens
-    WiFiCredential wifiAp = {"BruceNet", "brucenet"};
+    WiFiCredential wifiAp = {"RiftHack", "rifthack"};
     std::map<String, String> wifi = {};
     std::set<String> evilWifiNames = {};
     String wifiMAC = ""; //@IncursioHack
@@ -94,7 +94,7 @@ public:
     std::vector<String> disabledMenus = {};
 
     std::vector<QrCodeEntry> qrCodes = {
-        {"Bruce AP",   "WIFI:T:WPA;S:BruceNet;P:brucenet;;"},
+        {"Bruce AP",   "WIFI:T:WPA;S:RiftHack;P:RiftHack;;"},
         {"Bruce Wiki", "https://github.com/pr3y/Bruce/wiki"},
         {"Bruce Site", "https://bruce.computer"            },
         {"Rickroll",   "https://youtu.be/dQw4w9WgXcQ"      }

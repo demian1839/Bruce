9c462e900c242b7161ddaea84deb80d9b19b1f31
demian1839 <201471189+demian1839@users.noreply.github.com>
2026-09-18 08:52:16 +0000

    jo

--- src/core/serial_commands/storage_commands.cpp
+++ src/core/serial_commands/storage_commands.cpp
@@ -266,7 +266,6 @@ uint32_t ymodemReceiveCallback(cmd *c) {
     uint32_t totalBytes = 0;
     uint32_t expectedFileSize = 0;
     bool firstBlock = true;
-    bool headerReceived = false;
 
     // Clear any existing data in serial buffer and feed watchdog
     while (serialDevice->available()) {

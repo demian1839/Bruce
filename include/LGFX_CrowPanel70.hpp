0ba9138c8feb474ad8423a1bca2f2eda9484b47f
demian1839 <201471189+demian1839@users.noreply.github.com>
2026-09-19 14:41:06 +0200

    PCLK auf 10 MHz (24ms Zeichentakt)

--- include/LGFX_CrowPanel70.hpp
+++ include/LGFX_CrowPanel70.hpp
@@ -37,7 +37,7 @@ public:
       cfg.pin_hsync   = 39; // HSYNC
       cfg.pin_pclk    = 0;  // PCLK
 
-      cfg.freq_write = 24000000; // 24 MHz PCLK (Elecrow V3.0, quelle: riftos rift_config.h)
+      cfg.freq_write = 10000000; // 10 MHz PCLK
       cfg.hsync_polarity    = 0;
       cfg.hsync_front_porch = 40;
       cfg.hsync_pulse_width = 48;
